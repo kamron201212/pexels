b140eaf46e64b19604acc3770293a9b6eb892088
kamron201212 <gulamalievkamron@gmail.com>
2026-08-03 11:32:11 +0500

    o

--- apikey.js
+++ apikey.js
@@ -140,13 +140,37 @@ function createPhotoCard(photo) {
 
   photoCard.innerHTML = `
     <img src="${photo.src.medium}" alt="${photo.alt || 'Фото'}" class="w-full h-auto rounded-lg z-10 block" style="aspect-ratio: ${photo.width} / ${photo.height};">
-    <div class="absolute bottom-0 left-0 right-0 backdrop-blur-[4px]  text-white text-lg p-2 rounded-b-lg z-20 flex justify-between items-center opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-opacity duration-300 delay-150 ease-out">
+    <div class="flex-col justify-between items-center absolute inset-0 z-20 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-out">
+    <div class="absolute inset-0 bg-black/20 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-out flex justify-beetween items-center">   
+      <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="inline-flex items-center justify-center text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none" type="button">
+        Dropdown button 
+        <svg class="group-hover:rotate-180 transition-transform duration-300 ease-in-out w-4 h-4 ms-1.5 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 9-7 7-7-7"/></svg>
+      </button>
+      <div id="dropdown" class="z-10 hidden bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-44">
+          <ul class="p-2 text-sm text-body font-medium" aria-labelledby="dropdownDefaultButton">
+            <li>
+              <a href="#" class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Dashboard</a>
+            </li>
+            <li>
+              <a href="#" class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Settings</a>
+            </li>
+            <li>
+              <a href="#" class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Earnings</a>
+            </li>
+            <li>
+              <a href="#" class="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded">Sign out</a>
+            </li>
+          </ul>
+      </div>
+    </div>
+    <div class="absolute bottom-0 left-0 right-0 backdrop-blur-lg  text-white text-lg p-2 rounded-b-lg z-20 flex justify-between items-center opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-opacity duration-300 delay-150 ease-out">
       <span class="truncate -translate-x-4 group-hover:translate-x-0 transition-transform duration-300 delay-130 ease-in-out">
         ${photo.photographer}
       </span>
       <div class="backdrop-blur-[4px] flex justify-center items-center rounded-full h-auto shrink-0 truncate translate-x-4 group-hover:translate-x-0 transition-transform duration-300 delay-150 ease-in-out group-hover:backdrop-blur-lg">
         <button type="button" class="download-btn border flex text-center items-center justify-center rounded-full w-[90px] h-32px] cursor-pointer text-white">Скачать</button>
       </div>
+    </div>
     </div>
   `;
 
