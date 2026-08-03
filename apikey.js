aa55a10ecd8c5fcde950040e7c202981c9e08b7d
kamron201212 <gulamalievkamron@gmail.com>
2026-08-03 12:17:58 +0500

    n

--- apikey.js
+++ apikey.js
@@ -173,7 +173,7 @@ function createPhotoCard(photo) {
           ${photo.photographer}
         </span>
         <div class="flex justify-center items-center shrink-0 translate-x-4 group-hover:translate-x-0 transition-transform duration-300 delay-150 ease-in-out">
-          <button type="button" class="download-btn flex items-center gap-1.5 rounded-full bg-[#54ca84] hover:bg-[#43b873] px-4 py-2 text-white text-sm font-semibold cursor-pointer transition-colors duration-200">
+          <button type="button" class="download-btn flex items-center gap-1.5 rounded-full px-4 py-2 text-white text-sm font-semibold cursor-pointer transition-colors duration-200">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg>
             Скачать
           </button>
